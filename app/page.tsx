22b586a2612eb1f767d4c5f1f18d7bb18b61aff2
Joseph Owonvwon <joseph.owonwo@gmail.com>
2024-12-21 00:24:03 +0100

    chore: added effect to path stroke

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import { AnimatingPath } from "./AnimatingPath";
 
 export default function Page() {
   return <div className="bg-[#111] min-h-[100svh]">
-    <div className="absolute top-0 right-0 flex bg-blue-200">
+    <div className="absolute top-0 left-0 flex">
       <div>
-        <AnimatingPath color="orange" width="auto" height="100svh" />
+        <AnimatingPath color="orange" width="26vh" height="100svh" />
       </div>
       <div>
-        <AnimatingPath color="blue" width="auto" height="100svh" />
+        <AnimatingPath color="blue" width="26vh" height="100svh" />
       </div>
     </div>
   </div>
